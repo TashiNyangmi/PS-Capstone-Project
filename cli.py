616d1ae8e17b1edbf46918d98f24f888b5ecf818
Tashi Tsering Gurung <hseb.tashi@gmail.com>
2023-06-29 14:34:46 -0400

    refactor spark_queries 1.0 | added docstring and type hinting to all functions

---
+++ cli.py
@@ -0,0 +1,14 @@
+def get_user_choice():
+    # Let users know what they can do.
+    print("\n[1] Display the transactions made by customers living in a given zip code for a given month and year.")
+    print("[2] Display the number and total values of transactions for a given type.")
+    print("[3] Display the total number and total values of transactions for branches in a given state.")
+    print("[4] Check the existing account details.")
+    print("[5] Modify the existing account details.")
+    print("[6] Generate a monthly bill for a credit card number.")
+    print("[7] Display the transactions made by a customer between two dates.")
+    print("[q] Quit.")
+
+    return input("What would you like to do?")
+
+print('You chose: ', get_user_choice())
