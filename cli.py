1f41803d73be89a963c3796266e3925a848d91a1
Tashi Tsering Gurung <hseb.tashi@gmail.com>
2023-06-29 19:14:42 -0400

    cli.py merged from branch cli

---
+++ cli.py
@@ -0,0 +1,24 @@
+# --- Display Formatting ---- #
+from header_box import print_header_box
+
+# --- display formatting --- #
+
+def get_user_choice():
+    # Let users know what they can do.
+    print("\nPlease enter a digit from [1-7] for the corresponding action:")
+    print()
+    print("[1] Display the transactions made by customers living in a given zip code for a given month and year.")
+    print("[2] Display the number and total values of transactions for a given type.")
+    print("[3] Display the total number and total values of transactions for branches in a given state.")
+    print("[4] Check the existing account details.")
+    print("[5] Modify the existing account details.")
+    print("[6] Generate a monthly bill for a credit card number.")
+    print("[7] Display the transactions made by a customer between two dates.")
+    print("[q] Quit.")
+
+    return input("What would you like to do?")
+
+
+# Build CLI
+print_header_box(header_text = 'CUSTOMER DATA MANAGEMENT SYSTEM', box_width = 100)
+print("you chose: ", get_user_choice()) # validate input in the next iteration
